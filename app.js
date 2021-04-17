9a5e37d02bb5108ea41ce4bb8545f8ca6feae41b
Jason <jasonpsmith180@gmail.com>
2021-04-17 17:19:51 -0600

    add switch function to handle the initial action

--- app.js
+++ app.js
@@ -29,11 +29,37 @@ function startPrompt() {
                     'Add a department',
                     'Add a role',
                     'Add an employee',
-                    'Update and employee role'
+                    'Update an employee role'
                 ]
             }
         ])
-}
+        .then(answer => {
+            switch (answer.action) {
+                case ('View all departments'):
+                    console.log(answer.action);
+                    break;
+                case ('View all roles'):
+                    console.log(answer.action);
+                    break;
+                case ('View all employees'):
+                    console.log(answer.action);
+                    break;
+                case ('Add a department'):
+                    console.log(answer.action);
+                    break;
+                case ('Add a role'):
+                    console.log(answer.action);
+                    break;
+                case ('Add an employee'):
+                    console.log(answer.action);
+                    break;
+                case ('Update an employee role'):
+                    console.log(answer.action);
+                    break;
+            }
+        })
+    }
+
 
 // Call start function
 startPrompt();
