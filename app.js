20cfed69deb2aa8cc29c7c88b94cf36b7ad5ac71
Jason <jasonpsmith180@gmail.com>
2021-04-17 16:34:38 -0600

    add start prompt

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 // dependencies
 const db = require('./db/connection');
+const inquirer = require('inquirer');
 
 // start DB connection
 db.connect(err => {
-    if (err)  throw err;
-    console.log('Database connected.');
+    if (err) throw err;
 });
+
+// Start User Prompts
+function startPrompt() {
+    console.log(
+        `
+        ==========================
+             Employee Tracker
+        ==========================
+        `
+    );
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'Please choose an action.',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update and employee role'
+                ]
+            }
+        ])
+}
+
+// Call start function
+startPrompt();
+
 
